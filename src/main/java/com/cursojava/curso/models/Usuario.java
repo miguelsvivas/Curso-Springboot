4d72c6bb2fcd703e0441a742b1ab6385e3735984
Miguel <vivasmiguel96@gmail.com>
2021-10-28 19:46:18 -0500

    segundo commit

---
+++ src/main/java/com/cursojava/curso/models/Usuario.java
@@ -0,0 +1,84 @@
+package com.cursojava.curso.models;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.*;
+
+
+@Entity
+@Table(name = "usuarios")
+@ToString @EqualsAndHashCode
+public class Usuario {
+
+    @Id
+    @GeneratedValue (strategy=GenerationType.IDENTITY)
+    @Getter  @Setter @Column (name = "id")
+    private long id;
+
+    @Getter  @Setter @Column (name = "nombre")
+    private String nombre;
+
+    @Getter  @Setter @Column (name = "apellido")
+    private String apellido;
+
+    @Getter  @Setter @Column (name = "email")
+    private String email;
+
+    @Getter  @Setter @Column (name = "telefono")
+    private String telefono;
+
+    @Getter  @Setter @Column (name = "password")
+    private String password;
+
+    /*
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }*/
+}
---
+++ src/main/resources/static/js/usuarios.js
@@ -0,0 +1,62 @@
+// Call the dataTables jQuery plugin
+$(document).ready(function() {
+   cargarUsuarios();
+  $('#usuarios').DataTable();
+  actualizarEmailUsuario();
+});
+
+function actualizarEmailUsuario(){
+    document.getElementById('txt-email-usuario').outerHTML = localStorage.email;
+}
+
+async function cargarUsuarios(){
+
+
+      const request = await fetch('api/usuarios', {
+        method: 'GET',
+        headers:getHeaders()
+      });
+      const usuarios = await request.json();
+
+
+
+      let listadoHTML = '';
+      for (let usuario of usuarios){
+
+      let botonEliminar = '<a heref="#" onclick="eliminarUsuario('+ usuario.id +')" class="btn btn-danger btn-circle btn-sm"><i class="fas fa-trash"></i></a>';
+
+      let telefonoTexto = usuario.telefono == null ? '-' : usuario.telefono;
+
+      let usuarioHTML = '<tr><td>'+usuario.id+'</td><td>'+usuario.nombre+' '+usuario.apellido+'</td><td>'+usuario.email+'</td><td>'+telefonoTexto+'</td><td>'+botonEliminar+'</td></tr>';
+
+      listadoHTML += usuarioHTML;
+      }
+
+
+
+      console.log(usuarios);
+      document.querySelector('#usuarios tbody').outerHTML = listadoHTML;
+}
+
+function getHeaders(){
+return {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        'Authorization': localStorage.token
+};
+}
+
+async function eliminarUsuario(id){
+
+         if(!confirm('¿Desea elminar este usuario?')){
+            return;
+         }
+
+          const request = await fetch('api/usuarios/' +id, {
+            method: 'DELETE',
+            headers: getHeaders()
+          });
+
+          location.reload()
+
+}
